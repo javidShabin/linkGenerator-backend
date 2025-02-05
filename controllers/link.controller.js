2960697b7fbd680570294dfdc27514e6fb163c0e
javid shabin <javid.main.act@gmail.com>
2025-02-05 23:31:13 +0530

    Complete the link generating function and save to databse

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -6,8 +6,12 @@ import {
   generateSlug,
   generateWhatsAppLink,
 } from "../utils/generateLink.js";
+import userSchema from "../models/user.model.js";
+import linkSchema from "../models/link.model.js";
 import { linkGeneratingValidation } from "../validators/link.validation.js";
+import { success } from "../shared/response.js";
 
+// Generate whatsapp link including brand page and short link
 export const generateLink = async (req, res, next) => {
   try {
     const userId = req.user.id; // Get user id from authentication
@@ -22,7 +26,51 @@ export const generateLink = async (req, res, next) => {
     const whatsappLink = generateWhatsAppLink(phone, message);
     //  Generate short link using slug
     const shortLink = generateShortLink(slug);
-    
+
+    // Declare the two variable with null for store the base url and user id
+    let brandedPageUrl = null;
+    let userIds = null;
+
+    // Find user by user id
+    const user = await userSchema.findById(userId);
+    if (!user) return next(new AppError("User not found", 404));
+
+    // Check the user is pro or not
+    if (user.isPro) {
+      if (!user.id) {
+        return next(
+          new AppError(
+            "You must become premium user for generate brand page link",
+            400
+          )
+        );
+      }
+
+      userIds = user.id;
+      brandedPageUrl = `${process.env.CLIENT_URL}/${userIds}`;
+    }
+    // Create new link with the details
+    const newLink = new linkSchema({
+      userId,
+      slug,
+      phone,
+      message,
+      whatsappLink,
+      brandedPageUrl,
+      shortLink,
+      username: user.userName,
+    });
+    // Save the link
+    await newLink.save();
+
+    const data = {
+      slug: newLink.slug,
+      whatsappLink: newLink.whatsappLink,
+      brandedPageUrl: newLink.brandedPageUrl,
+      shortLink: newLink.shortLink,
+    };
+
+    success(res, data, "Link generated successfully");
   } catch (error) {
     next(error);
   }
