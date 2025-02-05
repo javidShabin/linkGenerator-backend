2d02fe12d955c8ca62b1b316fffe198c35df8b2b
javid shabin <javid.main.act@gmail.com>
2025-02-05 23:22:29 +0530

    Generate slug , whatsapp link, and short link

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -1,17 +1,29 @@
-
 // *************************************************************
 // ****************** Whatsapp link generating controllers *****************
 
-import { linkGeneratingValidation } from "../validators/link.validation.js"
+import {
+  generateShortLink,
+  generateSlug,
+  generateWhatsAppLink,
+} from "../utils/generateLink.js";
+import { linkGeneratingValidation } from "../validators/link.validation.js";
 
 export const generateLink = async (req, res, next) => {
-    try {
-        const userId = req.user.id; // Get user id from authentication
-        // Validate the data first
-        linkGeneratingValidation(req.body)
-        // Destructer the details from request body
-         const { phone, message, customSlug } = req.body;
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const userId = req.user.id; // Get user id from authentication
+    // Validate the data first
+    linkGeneratingValidation(req.body);
+    // Destructer the details from request body
+    const { phone, message, customSlug } = req.body;
+
+    // generate the slug
+    const slug = generateSlug(customSlug);
+    // Generate the whatsapp link
+    const whatsappLink = generateWhatsAppLink(phone, message);
+    //  Generate short link using slug
+    const shortLink = generateShortLink(slug);
+    
+  } catch (error) {
+    next(error);
+  }
+};
