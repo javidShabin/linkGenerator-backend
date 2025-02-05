b1d0a71f9168eab3fddfa1ad4ab4170b1794c3ff
javid shabin <javid.main.act@gmail.com>
2025-02-05 22:50:45 +0530

    Create generate short link function in utils folder

--- utils/generateLink.js
+++ utils/generateLink.js
@@ -8,3 +8,7 @@ export const generateWhatsAppLink = (phoneNumber, message) => {
   const encodedMessage = encodeURIComponent(message || "");
   return `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 };
+
+export const generateShortLink = (slug) => {
+  return `${process.env.CLIENT_URL}/s/${slug}`;
+};
