3dc0850e6b46f6fed1af12d7bbd35012f9f20744
javid shabin <javid.main.act@gmail.com>
2025-08-03 11:21:11 +0530

    Create the temp user model and user model temp is store include the otp and expire temporary

---
+++ models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isPro: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    profileImg: {
+      type: String,
+      default:
+        "//ui-avatars.com/api/?name=User&background=0D8ABC&color=fff&size=128",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "pro"],
+      default: "user",
+    },
+    lastLogin: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
