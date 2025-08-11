cb073bd0eeab4fb295a5e1ccdc5a56ba91c1db8b
javid shabin <javid.main.act@gmail.com>
2025-08-11 12:57:49 +0530

    Add deployed frontend url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 server.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://link-generator-frontend-orcin.vercel.app",
     credentials: true,
   })
 );
