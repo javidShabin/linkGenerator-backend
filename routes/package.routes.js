9e823e63e0a8d733a3facaa387815879f4e78727
javid shabin <javid.main.act@gmail.com>
2025-08-12 20:31:24 +0530

    Import the controller in routes

--- routes/package.routes.js
+++ routes/package.routes.js
@@ -1,23 +1,40 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { authorize } from "../middlewares/authorize.middleware.js";
-import { createPackage, deletePackage, getAllPackages, updatePackage } from "../controllers/package.controller.js";
+import {
+  createPackage,
+  deletePackage,
+  getAllPackages,
+  getAllPackagesAdmin,
+  togglePackageStatus,
+  updatePackage,
+} from "../controllers/package.controller.js";
 export const router = express.Router();
 
 // Create package route
-router.post("/create-package",authenticate, authorize("admin"), createPackage);
+router.post("/create-package", authenticate, authorize("admin"), createPackage);
 
 // Update package route
 router.put("/package/:id", authenticate, authorize("admin"), updatePackage);
 
 // Delete package route
-router.delete("/del-package/:id", authenticate, authorize("admin"), deletePackage);
+router.delete(
+  "/del-package/:id",
+  authenticate,
+  authorize("admin"),
+  deletePackage
+);
 
 // Get all packages route
-router.get("/get-packages", getAllPackages)
+router.get("/get-packages", getAllPackages);
 
 // Get all package for the admin route
-router.get("/get-packages-admin",authenticate, authorize("admin"), getAllPackagesAdmin)
+router.get(
+  "/get-packages-admin",
+  authenticate,
+  authorize("admin"),
+  getAllPackagesAdmin
+);
 
 // Toggle status activate and deactivate route
 router.patch("/packages/:id/toggle-status", togglePackageStatus);
